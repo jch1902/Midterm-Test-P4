c5a7b9144d2ee8201214a97010c0231be790f04e
Edgar V <54725200+evm100@users.noreply.github.com>
2019-09-10 14:03:09 -0700

    Update RandomLogic.java

--- MidtermTestP4/RandomLogic.java
+++ MidtermTestP4/RandomLogic.java
@@ -23,12 +23,6 @@ public class RandomLogic {
         String[] answers = {"==","=","!=","/="};
         
         System.out.println("Which operator do you use to test if two numbers are not equal to each other?");
-        /*
-        System.out.println("1. " + answers[answerAmount[0]]);
-        System.out.println("2. " + answers[answerAmount[1]]);
-        System.out.println("3. " + answers[answerAmount[2]]);
-        System.out.println("4. " + answers[answerAmount[3]]);
-        */
        
         for (int i=0; i<answerAmount.length; i++)
         {
