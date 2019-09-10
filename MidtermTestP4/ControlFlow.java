5b22ddceda514c324e24fb0bd3a5c11e79e17831
pdimagiba <42821744+pdimagiba@users.noreply.github.com>
2019-09-10 13:34:01 -0700

    question text

--- MidtermTestP4/ControlFlow.java
+++ MidtermTestP4/ControlFlow.java
@@ -1,5 +1,12 @@
 public class ControlFlow {
     public void askQuestion() {
         // Add code that asks the question and retrives an answer here
+        Scanner myObj = new Scanner(System.in);
+         
+        System.out.println("What happens when you don't initialize the loop control variable?");
+        System.out.println("a. Your compiler crashes.");
+        System.out.println("b. Java produces an error message.");
+        System.out.println("c. The code compiles but doesn't work correctly.");
+        System.out.println("d. The compiler ignores the error.");
     }
 }
